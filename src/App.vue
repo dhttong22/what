0ad42b73d877be1ecef0403e77499105a4dfce8c
dhttong <1404598387@qq.com>
2019-06-06 17:27:44 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>
+      这里是主页面
+    </h1>
+
+    <router-view></router-view>
+    
+<el-calendar v-model="value">
+</el-calendar>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  // import axios from 'axios'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+ export default {
+   mounted(){
+    this.$axios({
+    url: "https://api.github.com/users",
+    method: "GET",
+   }).then(res => {
+      console.log(res)
+    })
+   },
+
+    data() {
+      return {
+        value: new Date()
+      }
+    }
   }
-}
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
 </style>
---
+++ src/pages/Login.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        这里是登录页面
+        <div class="block">
+            <span class="demonstration">默认</span>
+        <el-slider v-model="value1"></el-slider>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+ data() {
+      return {
+        value1: 0,
+        value2: 50,
+        value3: 36,
+        value4: 48,
+        value5: 42
+      }
+    },
+    methods: {
+      formatTooltip(val) {
+        return val / 100;
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
